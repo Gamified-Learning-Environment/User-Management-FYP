3b9bc446f3ea0294edf8d876ec32c44dcf72c8ab
Shane Walsh <swalsh818@gmail.com>
2024-11-11 22:03:11 +0000

    Developed database test routes

--- app.py
+++ app.py
@@ -1,20 +1,27 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS # import CORS for cross-origin resource sharing
-
-import db
+import db # import db from db.py
 
 app = Flask(__name__) # Flask constructor takes the name of current module (__name__) as argument
 
+CORS(app) # enable CORS on the app
+
+# test data
+data = {
+    "name": "John",
+    "age": 30,
+    "city": "New York"
+}
+
 @app.route('/')  # route() decorator to tell Flask what URL should trigger the function
 def home():
-    print("successful connection")
+    print("successful connection to User Service")
     return "User Service"
 
-#test to insert data to the data base
-@app.route("/test")
-def test():
-    db.db.collection.insert_one({"name": "John"})
-    return "Connected to the data base!"
+@app.route('/data', methods=['POST'])
+def insert_data():
+    db.userdb.usercollection.insert_one(data)
+    return jsonify("Data inserted successfully" + str(data))
 
 if __name__ == "__main__":
     app.run(debug=True, port=8080)  # run() function of Flask class to run the application on the local development server
